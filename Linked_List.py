11b54e5286aaea281acee222401e0b2600b39002
Vijay Nagireddy <104459831+Vijaykumar933@users.noreply.github.com>
2024-06-05 00:36:28 +0530

    CGQ-1: Linked list constructor

---
+++ Linked_List.py
@@ -0,0 +1,18 @@
+"""
+This module consists of Linked list data structure with all the methods to implement
+"""
+class Node:
+    """To create a node"""
+    def __init__(self,value):
+        """Takes a value and creates a node"""
+        self.value = value
+        self.next = None
+
+class Linked_List:
+
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+
