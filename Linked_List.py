e5572c14e7a6e609ede21412264f3368dc3ca635
Vijay <vijaynagireddy76@gmail.com>
2024-06-22 17:35:05 +0530

    Linked_List: Append method added

--- Linked_List.py
+++ Linked_List.py
@@ -10,11 +10,12 @@ class Node:
 
 class Linked_List:
 
-    def __init__(self,value):
+    def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
 
     def print_list(self):
         temp = self.head
@@ -22,12 +23,26 @@ class Linked_List:
             print(temp.value)
             temp = temp.next
 
-    
+    def append(self, value):
+        """
+        appends a node at the end of linked list
+        value: value of a node
+        """
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
 
 
 
 if __name__ =="__main__":
     my_linked_list = Linked_List(1)
+    my_linked_list.print_list()
+    my_linked_list.append(2)
     my_linked_list.print_list()
 
     
